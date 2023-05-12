0a38e634c56c501f2f97436758d58b1425162295
Ebadm <ebad.malik99@gmail.com>
2023-05-12 17:27:46 +0300

    reset

--- TG3/swat_loader.py
+++ TG3/swat_loader.py
@@ -5,6 +5,7 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
+
 class swat_load_dataset(Dataset):
     def __init__(self, verbose=True, is_normalize=False, one_hot_encode=True, data_mode='Train'):
         super().__init__()
@@ -20,20 +21,26 @@ class swat_load_dataset(Dataset):
             shutil.unpack_archive('./swat-2015-data.zip', '.', 'zip')
             print("Successfully Unpacked")
 
-        window_size = 60  # Set this to the desired window size
+
+        window_size = 30  # Set this to the desired window size
+
 
         # df = pd.read_excel(xlsx_file, engine='openpyxl', header=1)  # Read the file
         x_train = np.load('swat-2015-data.npy')
         x_train = x_train[:7000, :]
 
+
         # Create sliding windows
         x_train_windows = self.create_windows(x_train, window_size)
+
 
         # Convert to PyTorch tensors
         self.x_train = torch.tensor(x_train_windows, dtype=torch.float32)
 
+
         if self.verbose:
             print("Shape of Data", self.x_train.shape)
+
 
     def create_windows(self, data, window_size):
         """
@@ -42,27 +49,35 @@ class swat_load_dataset(Dataset):
         num_samples, num_features = data.shape
         windowed_data = []
 
+
         # Slide the window over the number of samples (data points)
         for start in range(num_samples - window_size + 1):
             end = start + window_size
             window = data[start:end, :]
             windowed_data.append(window)
 
+
         windowed_data = np.stack(windowed_data)
 
+
         return windowed_data
+
 
     def __getitem__(self, index):
         # Get a window of data at the given index
         window = self.x_train[index]
 
+
         # Use a dummy target since it's not being used in your training loop
         dummy_target = torch.tensor(0)
 
+
         return window, dummy_target
+
 
     def __len__(self):
         return len(self.x_train)
+
 
     def download_url(self, url, destination):
         import requests
